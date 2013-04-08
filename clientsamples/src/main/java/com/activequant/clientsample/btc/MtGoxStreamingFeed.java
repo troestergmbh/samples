88c8ac1db2a1aba65420a5c931cccead07b76a88
activequant <ustaudinger@activequant.com>
2013-04-08 19:00:14 +0200

    adding message factory.

--- clientsamples/src/main/java/com/activequant/clientsample/btc/MtGoxStreamingFeed.java
+++ clientsamples/src/main/java/com/activequant/clientsample/btc/MtGoxStreamingFeed.java
@@ -1,22 +1,25 @@
 package com.activequant.clientsample.btc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.activequant.component.ComponentBase;
+import com.activequant.domainmodel.ETransportType;
+import com.activequant.interfaces.dao.IDaoFactory;
+import com.activequant.interfaces.transport.ITransportFactory;
+import com.activequant.messages.AQMessages.BaseMessage;
+import com.activequant.messages.MessageFactory;
+import com.activequant.utils.ArrayUtils;
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.ExchangeFactory;
 import com.xeiam.xchange.currency.Currencies;
 import com.xeiam.xchange.dto.marketdata.Ticker;
 import com.xeiam.xchange.mtgox.v1.MtGoxExchange;
 import com.xeiam.xchange.service.marketdata.polling.PollingMarketDataService;
-import com.xeiam.xchange.service.streaming.ExchangeEvent;
-import com.xeiam.xchange.service.streaming.StreamingExchangeService;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.activequant.component.ComponentBase;
-import com.activequant.domainmodel.MarketDataInstrument;
-import com.activequant.domainmodel.TradeableInstrument;
-import com.activequant.interfaces.dao.IDaoFactory;
-import com.activequant.interfaces.transport.ITransportFactory;
 
 /**
  * 
@@ -25,6 +28,8 @@ import com.activequant.interfaces.transport.ITransportFactory;
  */
 public class MtGoxStreamingFeed extends ComponentBase {
 
+	// to construct byte messages. 
+	private MessageFactory mf = new MessageFactory();
 	/**
 	 * 
 	 * @param transFac
@@ -41,10 +46,26 @@ public class MtGoxStreamingFeed extends ComponentBase {
         Exchange mtGox = ExchangeFactory.INSTANCE.createExchange(MtGoxExchange.class.getName());
         //StreamingExchangeService exchangeService = mtGox.getStreamingExchangeService();
         PollingMarketDataService marketDataService = mtGox.getPollingMarketDataService();
+        List<Double> bidPxList = new ArrayList<Double>();
+        List<Double> askPxList = new ArrayList<Double>();
+        List<Double> bidQList = new ArrayList<Double>();
+        List<Double> askQList = new ArrayList<Double>();
+        
         while ( true ) {
+        	// 
             Ticker ticker = marketDataService.getTicker(Currencies.BTC, Currencies.USD);
+            Double bid = ticker.getBid().getAmount().doubleValue();
+            Double ask = ticker.getAsk().getAmount().doubleValue();
+            // 
+            bidPxList.add(bid);
+            askPxList.add(ask);
+            bidQList.add(100000.0);
+            askQList.add(100000.0);
+            // 
             System.out.println(ticker);
             Thread.sleep(10000);
+            BaseMessage bm = mf.buildMds("MTGOX.BTC/USD", bidPxList, askPxList, bidQList, askQList, false);
+            transFac.getPublisher(ETransportType.MARKET_DATA, "MTGOX.BTC/USD").send(bm.toByteArray());
         }
     }
 
